8baac4de2d7cb8423a56f8b57ecb603414294ce0
Antonio Jankoski <aj28824@seeu.edu.mk>
2023-07-11 08:10:34 +0200

    fix(controller): improved error handling

--- src/dogs/controller.js
+++ src/dogs/controller.js
@@ -5,43 +5,46 @@ const { error } = require('console');
 const { response } = require('express');
 
 const getBreeds = (req, res) => {
-    pool.query(queries.getBreeds, (error, results) => {
-      if (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Internal server error' });
+  pool.query(queries.getBreeds, (error, results) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    const adjustedData = {};
+
+    for (let i = 0; i < results.rows.length; i++) {
+      const { breed, sub_breed } = results.rows[i];
+
+      if (Array.isArray(sub_breed)) {
+        adjustedData[breed] = sub_breed;
+      } else if (sub_breed) {
+        adjustedData[breed] = sub_breed
+          .replace(/[' "]/g, '')
+          .split(',');
+      } else {
+        adjustedData[breed] = [];
       }
+    }
 
-        const adjustedData = {};
+    const response = { message: adjustedData, status: 'success' };
 
-        for (let i = 0; i < results.rows.length; i++) {
-            const { breed, sub_breed } = results.rows[i];
-      
-            if (Array.isArray(sub_breed)) {
-              adjustedData[breed] = sub_breed;
-            } else if (sub_breed) {
-              adjustedData[breed] = sub_breed
-                .replace(/[' "]/g, '') 
-                .split(',');       
-            } else {
-              adjustedData[breed] = [];
-            }
-          }
-    
-        const response = { message: adjustedData, status: "success" };
-    
-        res.status(200).json(response);
-    })
-}
+    res.status(200).json(response);
+  });
+};
 
 const getRandomBreedImage = (req, res) => {
   const breed = req.params.breed;
-    pool.query(queries.getImage, [breed], (error, results) => {
-      if (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Internal server error' });
-      }
-      res.status(200).json({ message: results.rows[0].link, status: "success" })
-    });
+  pool.query(queries.getImage, [breed], (error, results) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+    if (results.rows.length === 0) {
+      return res.status(404).json({ error: 'Breed not found' });
+    }
+    res.status(200).json({ message: results.rows[0].link, status: 'success' });
+  });
 };
 
 const getRandomBreedImageWithSubBreed = (req, res) => {
@@ -55,33 +58,43 @@ const getRandomBreedImageWithSubBreed = (req, res) => {
     }
 
     const breedRow = results.rows[0];
-    const requestedSubBreed = breedRow.sub_breed.find(sb => sb === sub_breed);
-    pool.query(queries.getImageWithSubBreed, [breed, requestedSubBreed], (error, results) => {
+    if (!breedRow || !breedRow.sub_breed.includes(sub_breed)) {
+      return res.status(404).json({ error: 'Breed or sub-breed not found' });
+    }
+
+    pool.query(queries.getImageWithSubBreed, [breed, sub_breed], (error, results) => {
       if (error) {
         console.error(error);
         return res.status(500).json({ error: 'Internal server error' });
       }
-      res.status(200).json({ message: results.rows[0].link, status: "success" })
+      if (results.rows.length === 0) {
+        return res.status(404).json({ error: 'Image not found' });
+      }
+      res.status(200).json({ message: results.rows[0].link, status: 'success' });
     });
   });
 };
 
 const addBreed = (req, res) => {
-    const { breed, sub_breed } = req.body;
-    pool.query(queries.checkBreedExists, [breed], (error, results) => {
-      if(results.rows.length) {
-          res.send("Breed already exists.");
-      }
-
-    pool.query(queries.addBreed, [breed, sub_breed], (error, results) => {
+  const { breed, sub_breed } = req.body;
+  pool.query(queries.checkBreedExists, [breed], (error, results) => {
     if (error) {
       console.error(error);
       return res.status(500).json({ error: 'Internal server error' });
     }
+    if (results.rows.length) {
+      return res.status(400).json({ error: 'Breed already exists' });
+    }
 
-      res.status(201).send("Breed added successfully.");
+    pool.query(queries.addBreed, [breed, sub_breed], (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+
+      res.status(201).send('Breed added successfully.');
     });
-  })
+  });
 };
 
 const addImage = (req, res) => {
@@ -90,9 +103,8 @@ const addImage = (req, res) => {
     if (error) {
       console.error(error);
       return res.status(500).json({ error: 'Internal server error' });
-    };
-
-      res.status(201).send("Image added successfully.");
+    }
+    res.status(201).send('Image added successfully.');
   });
 };
 module.exports = {
